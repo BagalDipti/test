6431f51345844edc32c5ebfce9dc9dc380954ba1
Dipti Bagal <52457642+BagalDipti@users.noreply.github.com>
2021-07-14 15:54:06 +0530

    Create BasicTestCases.cpp

---
+++ BasicTestCases.cpp
@@ -0,0 +1,11 @@
+#include "BasicTestCases.hpp"
+
+auto BasicTestCases::SaveCharacterToMongoDb()
+{
+   return served::response::stock_reply(200, response);
+}
+auto BasicTestCases::DeleteHandler()
+{
+ return served::response::stock_reply(200, response);
+
+}
